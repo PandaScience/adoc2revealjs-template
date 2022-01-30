f440513f7dc1f3607020b23221eabd9156a23ced
René Wirnata <rene.wirnata@scandio.de>
2022-01-30 13:16:26 +0100

    feat: add PgUp/PgDown scripts

--- plugins-conf.js
+++ plugins-conf.js
@@ -5,3 +5,32 @@ pdfExportShortcut: 'E',
 skipFragmentsShowShortcut: 'V',
 // Shortcut for hiding all fragments
 skipFragmentsHideShortcut: 'C',
+
+// use e.g. https://keycode.info to find keycodes
+keyboard: {
+	// use PgUp and PgDown to change slides while skipping all fragments
+	33: function() {
+			let idx = Reveal.getIndices()
+			let routes = Reveal.availableRoutes()
+			if( routes.up ){
+				console.log("up")
+				Reveal.slide(idx.h, idx.v - 1);
+			} else if( routes.left ){
+				console.log("left")
+				Reveal.slide(idx.h - 1);
+			}
+			SkipFragments.showAll()
+		},
+	34: function() {
+			let idx = Reveal.getIndices()
+			let routes = Reveal.availableRoutes()
+			if( routes.down ){
+				console.log("down")
+				Reveal.slide(idx.h, idx.v + 1);
+			} else if( routes.right ){
+				console.log("right")
+				Reveal.slide(idx.h + 1);
+			}
+			SkipFragments.showAll()
+		},
+}
